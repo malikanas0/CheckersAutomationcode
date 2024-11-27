080284cbf6b85bb1c982adaf4580df9b60434fbd
Muhammad Anas <malikanassultan@gmail.com>
2024-11-27 22:49:57 +0500

    Both task complete without assertion

--- demoqa.cy.js
+++ demoqa.cy.js
@@ -1,16 +1,18 @@
 import { demoqaverifyaddnewuser } from "../../utlis/demoqalocator"
+import { verifyeditfunctionality } from "../../utlis/demoqalocator";
 Cypress.on('uncaught:exception', (err, runnable) => {
     return false
     });
+    beforeEach(()=>{cy.visit('https://demoqa.com/')})
 describe('demoqa site automation',()=>{
     it('verify user can enter new data into the table',()=>{
-        cy.visit('https://demoqa.com/')
+        //cy.visit('https://demoqa.com/')
         cy.url().should('include','com')
         cy.get(demoqaverifyaddnewuser.navigatetoelement).eq(0).click()
         cy.get(demoqaverifyaddnewuser.clickonwebtales).click()
         cy.get(demoqaverifyaddnewuser.clickonaddbutton).click()
         cy.get(demoqaverifyaddnewuser.adduserfirstname).type('Alden')
-        cy.get(demoqaverifyaddnewuser.adduserlastname).type('Cantrell')
+        cy.get(demoqaverifyaddnewuser.adduserlastname).type('Cantrell').should('have.text', 'Cantrell')
         cy.wait(3000)
         cy.get(demoqaverifyaddnewuser.adduserEmail).type('test@test.com')
         cy.wait(3000)
@@ -19,5 +21,13 @@ describe('demoqa site automation',()=>{
         cy.get(demoqaverifyaddnewuser.addusersalary).type('12345')
         cy.get(demoqaverifyaddnewuser.adduserdepartment).type('QA')
         cy.get(demoqaverifyaddnewuser.clickonsubmitbtn).click()
+    })
+    it('verify user can edit row in a table',()=>{
+        cy.get(verifyeditfunctionality.navigatetoelement).eq(0).click()
+        cy.get(verifyeditfunctionality.clickonwebtales).click()
+        cy.get(verifyeditfunctionality.clickonsecroweditbox).click()
+        cy.get(verifyeditfunctionality.clearfirstname).clear().type('Gerimedica')
+        cy.get(verifyeditfunctionality.clearlastname).clear().type('BV')
+        cy.get(verifyeditfunctionality.clickonsubmitbtn).click()
     })
 })
